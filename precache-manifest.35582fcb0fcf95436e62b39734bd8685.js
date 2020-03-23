a0e1ea1454fd7ab20b743cf0b61d5d566c681d24
bhumijgupta <bhumijgupta@gmail.com>
2020-03-24 00:21:19 +0530

    Updates

---
+++ precache-manifest.35582fcb0fcf95436e62b39734bd8685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76266d72a2cfa13cdcdcc32a86a7fa11",
+    "url": "/sih2020-ps-viewer/index.html"
+  },
+  {
+    "revision": "aa19ee6432867d47c41b",
+    "url": "/sih2020-ps-viewer/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6610199b07f0f181d713",
+    "url": "/sih2020-ps-viewer/static/css/main.087c7936.chunk.css"
+  },
+  {
+    "revision": "aa19ee6432867d47c41b",
+    "url": "/sih2020-ps-viewer/static/js/2.2d03a5b5.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/sih2020-ps-viewer/static/js/2.2d03a5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6610199b07f0f181d713",
+    "url": "/sih2020-ps-viewer/static/js/main.b0cca2f5.chunk.js"
+  },
+  {
+    "revision": "72d5e1ecbd014ea65a48",
+    "url": "/sih2020-ps-viewer/static/js/runtime-main.b0733e49.js"
+  }
+]);
